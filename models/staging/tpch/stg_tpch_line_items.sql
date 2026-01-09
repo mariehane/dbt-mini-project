862613e0c0a6c87685cc50adba9dbeac51c71718
Ludvig <ludvigkarstens@hotmail.com>
2026-01-09 23:15:51 +0100

    Update syntax, update dbt-utils to 1.3.3

--- models/staging/tpch/stg_tpch_line_items.sql
+++ models/staging/tpch/stg_tpch_line_items.sql
@@ -7,8 +7,7 @@ with source as (
 renamed as (
 
     select
-    
-        {{ dbt_utils.surrogate_key(
+        {{ dbt_utils.generate_surrogate_key(
             ['l_orderkey', 
             'l_linenumber']) }}
                 as order_item_key,
